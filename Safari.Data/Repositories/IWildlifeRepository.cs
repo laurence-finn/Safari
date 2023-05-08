6f1033ec0d7f11e1983d1e0f3107c991d10c748b
Laurence Finn <laurence.c.finn@gmail.com>
2023-05-08 15:50:21 -0500

    Create a generic IWildlifeRepository interface and store it in the new "Repositories" folder in Safari.Data.

---
+++ Safari.Data/Repositories/IWildlifeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safari.Data
+{
+    public interface IWildlifeRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIDAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
