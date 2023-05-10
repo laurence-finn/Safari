56cf4c165328a0a5e43c64f7288fa0372462d3d0
Laurence Finn <laurence.c.finn@gmail.com>
2023-05-09 21:26:40 -0500

    whoops

--- Safari.Web/Program.cs
+++ Safari.Web/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using Safari.Data;
+using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connection = builder.Configuration.GetConnectionString("WildlifeDataConnection");
+builder.Services.AddDbContext<WildlifeDataContext>(options =>
+        options.UseSqlServer(connection));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -13,6 +20,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
+
+WildlifeDataContext wild = new WildlifeDataContext(connection);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
