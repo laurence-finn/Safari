805de016364fff0dae95cc263209cd742dcf6f8d
Laurence Finn <laurence.c.finn@gmail.com>
2023-05-11 20:20:29 -0500

    Add files for areas of the website (Submit Image, View Animals, Map). Delete the logo.jpg until it is remade later.

---
+++ Safari.Web/Pages/Map.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Safari.Web.Pages
+{
+    public class MapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Safari.Web/Pages/ViewAnimals.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Safari.Data;
+
+namespace Safari.Web.Pages.Animals
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WildlifeDataContext _context;
+
+        public IndexModel(WildlifeDataContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Animal> Animal { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Animals != null)
+            {
+                Animal = await _context.Animals
+                .Include(a => a.AnimalType)
+                .Include(a => a.DietType).ToListAsync();
+            }
+        }
+    }
+}
