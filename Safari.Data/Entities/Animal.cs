c60c826eb00fa6aa89d1ed1cc4eee71bb9bebc1f
Laurence Finn <laurence.c.finn@gmail.com>
2023-05-29 23:43:24 -0500

    Add proper labels to the columns in ViewAnimals.cshtml. Create a ViewAnimalModel to serve as the data model for the ViewAnimals page.

--- Safari.Data/Entities/Animal.cs
+++ Safari.Data/Entities/Animal.cs
@@ -15,6 +15,7 @@ public partial class Animal
     public int AnimalId { get; set; }
 
     [StringLength(50)]
+    [Display (Name = "Name")]
     public string Name { get; set; } = null!;
 
     [Column("AnimalTypeID")]
@@ -25,25 +26,32 @@ public partial class Animal
 
     [LessThanOrEqual(nameof(MaxWeight), ErrorMessage = "Minimum Weight must be less than or equal to Maximum Weight.")]
     [Column(TypeName = "decimal(6, 2)")]
+    [Display (Name = "Min Weight")]
     public decimal? MinWeight { get; set; }
 
     [GreaterThanOrEqual(nameof(MinWeight), ErrorMessage = "Maximum Weight must be greater than or equal to Minimum Weight.")]
     [Column(TypeName = "decimal(6, 2)")]
+    [Display (Name = "Max Weight")]
     public decimal? MaxWeight { get; set; }
 
     [Column(TypeName = "decimal(6, 2)")]
+    [Display (Name = "Avg. Height")]
     public decimal? Height { get; set; }
 
+    [Display (Name = "Endangered?")]
     public bool? IsEndangered { get; set; }
 
     [Range(1, 200, ErrorMessage ="Average life span must be greater than 0 and less than 200 years.")]
+    [Display (Name = "Avg. Lifespan")]
     public int? AverageLifeSpan { get; set; }
 
     [ForeignKey("AnimalTypeId")]
     [InverseProperty("Animal")]
+    [Display(Name = "Type")]
     public virtual AnimalType? AnimalType { get; set; }
 
     [ForeignKey("DietTypeId")]
     [InverseProperty("Animal")]
+    [Display (Name = "Diet")]
     public virtual DietType? DietType { get; set; }
 }
---
+++ Safari.Web/ViewModels/ViewAnimalModel.cs
@@ -0,0 +1,11 @@
+﻿using Safari.Data;
+
+namespace Safari.Web.ViewModels
+{
+    public class ViewAnimalModel
+    {
+        public string States { get; set; } = default!;
+
+
+    }
+}
