0d26f5ac62e9f07ac830f0db789d75eaf9cdc929
Laurence Finn <laurence.c.finn@gmail.com>
2023-06-19 15:03:30 -0500

    Create the code for the Interactive Map page.

--- Safari.Web/Pages/Map.cshtml.cs
+++ Safari.Web/Pages/Map.cshtml.cs
@@ -1,12 +1,39 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Safari.Data;
+using Safari.Web.ViewModels;
 
-namespace Safari.Web.Pages
+namespace Safari.Web.Pages;
+
+public class MapPageModel : PageModel
 {
-    public class MapModel : PageModel
+    private readonly WildlifeDataContext _context;
+    private readonly IMapper _mapper;
+
+    public MapPageModel(WildlifeDataContext context, IMapper mapper)
     {
-        public void OnGet()
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public IList<AnimalViewModel> Animal { get; set; } = default!;
+
+    public async Task OnGetAsync()
+    {
+        if (_context.Animal != null)
         {
+            var animals = await _context.Animal
+            .Include(a => a.AnimalType)
+            .Include(a => a.DietType)
+            .Include(a => a.AnimalPic)
+            .Include(a => a.AnimalState)
+                .ThenInclude(AnimalState => AnimalState.State)
+            .ToListAsync();
+
+            // Map Animal to AnimalViewModel (populate data from Animal to AnimalViewModel)
+            Animal = _mapper.Map<IList<AnimalViewModel>>(animals);
         }
     }
 }
